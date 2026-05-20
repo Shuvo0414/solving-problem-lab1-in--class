3fd850cdb64aebb8eb4c68ec6824fa1303e4dde4
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-05-20 13:21:03 +0600

    solved 4th problem ,traverse the given 3×3 two-dimensional array and print all elements of the matrix in the correct formate

---
+++ 4th_solve_problem.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Function declaration
+void printMatrix(int (*ptr)[3], int rows, int cols);
+
+int main()
+{
+    // 3x3 two-dimensional array
+    int matrix[3][3] =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}};
+
+    // Call function and pass matrix
+    printMatrix(matrix, 3, 3);
+
+    return 0;
+}
+
+// Function to print matrix using pointers
+void printMatrix(int (*ptr)[3], int rows, int cols)
+{
+    // Traverse rows
+    for (int i = 0; i < rows; i++)
+    {
+        // Traverse columns
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", *(*(ptr + i) + j));
+        }
+
+        // Move to next line after each row
+        printf("\n");
+    }
+}
